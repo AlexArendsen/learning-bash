af8902d67ef70013249d0be5e089373b329ceccc
Alex Arendsen <alex.arendsen.7@gmail.com>
2015-10-14 17:07:19 -0400

    Wrote the histogram program

--- bash-histo.sh
+++ bash-histo.sh
@@ -1,3 +1,42 @@
 #!/bin/bash
 
-# TODO -- Still working on this one...
+# Two Perl functions for character-integer conversion
+chr(){
+  echo `perl -CA -le 'print chr shift' $1`
+}
+
+ord(){
+  echo `perl -CA -le 'print ord shift' $1`
+}
+
+echo -n "Enter a string: "
+read src
+
+# Convert to lowercase
+src=${src,,}
+base=`ord "a"`
+
+# Initialize array
+for (( i=0; i<26; i++ )); do
+  let freq[i]=0
+done
+
+# Fill array
+for (( i=0; i<${#src}; i++ )); do
+  code=`ord "${src:$i:1}"`
+  idx=$(( code - base ))
+  if (( idx > -1 )) && (( idx < 26 )); then
+    let freq[idx]++
+  fi
+done
+
+# Output
+for (( i = 0; i < 26; i++ )); do
+  char=`chr $(( i + base ))`
+  el="${freq[$i]}"
+  echo -n "${char^^} | "
+  for (( j = 0; j < el; j++ )); do
+    echo -n "*"
+  done
+  echo
+done
